50d2ed07baa063cf758bb73b5e1bffd9521b5ed9
McCrearyD <mccreary@dyllan.ai>
2021-02-24 21:57:30 -0800

    passwords with new password button

--- pages/dashboard/passwords.js
+++ pages/dashboard/passwords.js
@@ -1,5 +1,24 @@
+import React from 'react';
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import ProTip from '../../src/ProTip';
+import Copyright from '../../src/Copyright';
+import New from '../../components/new';
+
 export default function Passwords() {
     return (
-        <h1>PASSWORDS</h1>
+        <Container maxWidth="sm">
+        <Box my={4}>
+            <Box my={4}>
+            <Typography variant="h4" component="h1" gutterBottom>
+            Passwords
+          </Typography>
+            </Box>
+          <New entryType='Password' />
+          <ProTip />
+          <Copyright />
+        </Box>
+      </Container>
     )
 }
